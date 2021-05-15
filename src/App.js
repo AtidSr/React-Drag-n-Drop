e592d6982f41bb6cdc44e960dca9a087e07015a5
Atid Sr <atid.ssri@gmail.com>
2021-05-15 11:02:03 +0700

    add template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './css/main.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="file-upload">
+        <h2>Image Drag & Drop & Preview</h2>
+        <form className="" encType= 'multipart/form-data'>
+            <div className="custom-form-group" >
+                <input type="text" name="title" placeholder="Title" />
+            </div>
+            <div className="custom-form-group">
+                <input type="text" name="desc" placeholder="Description" />
+            </div>
+            <div className="custom-form-group">
+                <div className="custom-file-drop-area ">
+                    <input type="file"name="photos" placeholder="Enter photos" multiple="true" id="filephotos"/>
+                    <label htmlFor="filephotos">Drag & Drop</label>
+                </div>
+                <div className="custom-file-preview">
+                    <div className="prev-img">
+                        <span>&times;</span>
+                        <img src="https://picsum.photos/id/237/200/300" alt="asd"/>
+                    </div>
+                    <div className="prev-img">
+                        <span>&times;</span>
+                        <img src="https://picsum.photos/id/237/200/300" alt="asd"/>
+                    </div>
+                </div>
+            </div>
+            <button type="submit" className="btn-submit">Submit</button>
+        </form>
+    </div>
     </div>
   );
 }
