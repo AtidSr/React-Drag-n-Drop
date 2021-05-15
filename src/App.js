d0dac9e922f1310b15144a14cd2a1d9f19b83988
Atid Sr <atid.ssri@gmail.com>
2021-05-15 11:33:09 +0700

    biding data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './css/main.css'
 
 function App() {
+  const [post, setPost] = useState({
+    title:'',
+    desc: '',
+    photos:[]
+  })
+
+
+  const {title,desc, photos} = post
+  const handleChange = e => {
+    setPost({
+      ...post,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleFileChange = e => {
+    let files = e.target.files
+    let photoArray = []
+
+    for(let file of files) {
+      let reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.addEventListener('load', () => {
+        let fileObj = {
+          name: file.name,
+          type: file.type,
+          size: file.size,
+          src: reader.result
+        }
+        photoArray.push(fileObj)
+        setPost({...post, photos:[...photos, ...photoArray] })
+      })
+    }
+  }
   return (
     <div className="App">
           <div className="file-upload">
         <h2>Image Drag & Drop & Preview</h2>
         <form className="" encType= 'multipart/form-data'>
             <div className="custom-form-group" >
-                <input type="text" name="title" placeholder="Title" />
+                <input type="text" name="title" placeholder="Title" value={title} onChange={handleChange}/>
             </div>
             <div className="custom-form-group">
-                <input type="text" name="desc" placeholder="Description" />
+                <input type="text" name="desc" placeholder="Description" value={desc} onChange={handleChange}/>
             </div>
             <div className="custom-form-group">
                 <div className="custom-file-drop-area ">
-                    <input type="file"name="photos" placeholder="Enter photos" multiple="true" id="filephotos"/>
+                    <input type="file"name="photos" placeholder="Enter photos" multiple="true" id="filephotos" onChange={handleFileChange}/>
                     <label htmlFor="filephotos">Drag & Drop</label>
                 </div>
                 <div className="custom-file-preview">
